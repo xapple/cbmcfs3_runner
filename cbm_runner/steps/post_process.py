2a95d097f2c9c2692ce48ecd1631e2914fc627c9
Paul Rougieux <paul.rougieux@ec.europa.eu>
2019-02-26 15:55:12 +0100

    Generate the updated inventory from a simulation result

--- cbm_runner/steps/post_process.py
+++ cbm_runner/steps/post_process.py
@@ -58,8 +58,57 @@ class PostProcessor(object):
         # Remove the confusing name #
         del classifiers.columns.name
         return classifiers
-    
+   
+    @property_cached
+    def coefficients(self):
+        csv = """ID,species,C,DB,Harvest_Gr
+               76,AA,0.5,0.4,Con
+               77,HP,0.5,0.4,Con
+               79,PS,0.5,0.42,Con
+               80,FS,0.5,0.58,Broad
+               81,QR,0.5,0.58,Broad
+               83,LD,0.5,0.46,Con
+               86,OB,0.5,0.5,Broad
+               87,OC,0.5,0.4,Con"""
+        import StringIO
+        csv = StringIO.StringIO(csv)
+        return(pandas.read_csv(csv))
+
+    @property_cached
+    def bef_ft(self):
+        pool = self.sim_result["tblPoolIndicators"].set_index('UserDefdClassSetID')
+        bef_ft = pool.join(self.classifiers, on="UserDefdClassSetID")
+        cols_sum = {'SW_Merch'  :'sum',
+                    'SW_Foliage':'sum',
+                    'SW_Other'  :'sum',
+                    'HW_Merch'  :'sum',
+                    'HW_Foliage':'sum',
+                    'HW_Other'  :'sum',
+                    'SW_Coarse' :'sum',
+                    'SW_Fine'   :'sum',
+                    'HW_Coarse' :'sum',
+                    'HW_Fine'   :'sum'}
+        bef_ft = bef_ft.groupby("forest_type").agg(cols_sum)
+        bef_ft['Tot_Merch']  = bef_ft.SW_Merch + bef_ft.HW_Merch
+        bef_ft['Tot_ABG']    = bef_ft.SW_Merch + bef_ft.HW_Merch + \
+                             bef_ft.SW_Foliage + bef_ft.HW_Foliage + \
+                             bef_ft.HW_Other + bef_ft.SW_Other
+        bef_ft['BG_Biomass'] = bef_ft.SW_Coarse + bef_ft.SW_Fine + \
+                             bef_ft.HW_Coarse + bef_ft.HW_Fine
+        bef_ft['BEF_Tot']    = (bef_ft.Tot_ABG + bef_ft.BG_Biomass) / bef_ft.Tot_ABG
+        return bef_ft 
+
     @property_cached
     def predicted_inventory(self):
-        return 0
-
+        age_indicators = self.sim_result["tblAgeIndicators"]
+        inv = age_indicators.set_index('UserDefdClassSetID').join(self.classifiers, on='UserDefdClassSetID')
+        inv = inv.reset_index().set_index('forest_type').join(self.bef_ft, on='forest_type')
+        inv = inv.reset_index().set_index('species').join(self.coefficients.set_index('species'), on='species')
+        inv = inv.reset_index()
+        inv = inv[['species', 'forest_type',
+                   'AveAge', 'TimeStep', 'Area', 
+                   'Biomass', 'BEF_Tot',
+                   'DB']]
+        inv['Merch_C_ha'] = inv.Biomass / inv.BEF_Tot
+        inv['Merch_Vol_ha'] = inv.Merch_C_ha / inv.DB
+        return inv 
