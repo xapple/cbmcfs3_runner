b15d45591ce80b9c9da5e502dcdf899f3c3564db
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2019-03-08 10:08:19 +0100

    Change all countries

--- cbm_runner/all_countries.py
+++ cbm_runner/all_countries.py
@@ -7,21 +7,18 @@ import inspect
 from autopaths.dir_path import DirectoryPath
 
 # Internal modules #
-from cbm_runner.country import Country
+from cbm_runner.continent import Continent
 
 # Constants #
-cbm_data_repos  = DirectoryPath("/repos/cbm_data/")
+cbm_data_repos = DirectoryPath("/repos/cbm_data/")
 
 ###############################################################################
-# Retrieve list of all countries #
-all_countries = [Country(d) for d in cbm_data_repos.flat_directories if 'orig' in d]
-
-# For backwards compatibility #
-all_runners = all_countries
+# Create list of all countries #
+continent = Continent(cbm_data_repos)
 
 ###############################################################################
 # Black magic: set a variable with the country code in the global namespace
 # so that print(AT) for instance will show the Runner() instance for Austria
 # And you can do `from cbm_runner.all_countries import AT`
-for country in all_countries:
+for country in continent.all_countries:
     inspect.currentframe().f_locals[country.country_iso2] = country
