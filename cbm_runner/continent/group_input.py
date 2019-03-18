063cc097e578af23f164a2f62a509e4edff46e2a
Paul Rougieux <paul.rougieux@gmail.com>
2019-03-18 11:04:57 +0000

    Print only countries which have a difference

--- cbm_runner/continent/group_input.py
+++ cbm_runner/continent/group_input.py
@@ -10,11 +10,15 @@ from autopaths.auto_paths import AutoPaths
 # Internal modules #
 
 ###############################################################################
+
+
 class GroupInput(object):
     """
-    Concatenate input table in one single table for all countries available. 
-    Also provide a dictionary of data frames to investigate inconsistent conlumn names between the different countries. 
-    The compare_column_names method can be used to print differences in column names compared to a reference country
+    Concatenate input table in one single table for all countries available.
+    Also provide a dictionary of data frames
+    to investigate inconsistent conlumn names between the different countries.
+    The compare_column_names method can be used to print
+    differences in column names compared to a reference country
     (defaults to AT).
     """
 
@@ -24,17 +28,27 @@ class GroupInput(object):
 
     def as_dict(self, name):
         """A dictionary of data frames, with country iso 2 code as keys."""
-        dist_all = [(r.country_iso2, r.csv_to_xls.read_csv(name)) for r in self.parent.all_countries]
+        dist_all = [(r.country_iso2,
+                     r.csv_to_xls.read_csv(name))
+                    for r in self.parent.all_countries]
         return OrderedDict(dist_all)
 
     def as_concat_df(self, name):
-        """A concatenated data frame containing disturbance tables for all countries."""
+        """A concatenated data frame containing tables for all countries."""
         return pandas.concat(self.as_dict(name))
 
-    def compare_column_names(self, name, country_ref = 'AT'):
-        """Print differences in column names, compared to a reference country"""
-        dict_of_df = self.as_dict(name) 
+    def compare_column_names(self, name, country_ref='AT'):
+        """Print differences in column names,
+        compared to a reference country"""
+        print("(Specific to this country, present in reference country: " +
+              country_ref + ")")
+        dict_of_df = self.as_dict(name)
+        comparison = OrderedDict()
         for country, table in dict_of_df.items():
-            print("\n"+country)
-            print(dict_of_df[country_ref].columns.difference(table.columns))
-            print(table.columns.difference(dict_of_df[country_ref].columns))
+            specific_to_this = list(table.columns.difference(dict_of_df[country_ref].columns))
+            present_in_ref = list(dict_of_df[country_ref].columns.difference(table.columns))
+            comparison[country] = (specific_to_this, present_in_ref)
+        for country, table in comparison.items():
+            if(table[0] or table[1]):
+                print(country)
+                print(table)
