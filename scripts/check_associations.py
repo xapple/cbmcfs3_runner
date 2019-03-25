9cdec890895ba5c4a56160fa36dc10acd4e836a4
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2019-03-25 14:10:08 +0100

    Script to check associations.csv

---
+++ scripts/check_associations.py
@@ -0,0 +1,21 @@
+"""
+A script to test if the associations.csv are good.
+
+Typically you would run this file from a command line like this:
+
+     ipython.exe -i -- /deploy/cbm_runner/scripts/check_associations.py
+"""
+
+# Built-in modules #
+
+# Third party modules #
+
+# First party modules #
+
+# Internal modules #
+from cbm_runner.all_countries import continent
+
+# Constants #
+
+###############################################################################
+for country in continent: country.orig_to_csv.associations_parser.list_missing()
