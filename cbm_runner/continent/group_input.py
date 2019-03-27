beb50e28309dee9a870075e1fc08c6e668d2a293
Paul Rougieux <paul.rougieux@gmail.com>
2019-03-27 15:27:03 +0000

    Remove index from concatenated df and make it a country_iso2 column

--- cbm_runner/continent/group_input.py
+++ cbm_runner/continent/group_input.py
@@ -39,7 +39,10 @@ class GroupInput(object):
 
     def as_concat_df(self, name):
         """A concatenated data frame containing tables for all countries."""
-        return pandas.concat(self.as_dict(name))
+        df = pandas.concat(self.as_dict(name)) 
+        df = df.reset_index(level=0)
+        df.rename(columns={'level_0': 'country_iso2'})
+        return df
 
     def compare_column_names_in_dict_of_df(self, dict_of_df, key_ref='AT'):
         """Compare column names in a dictionnary of data frames
