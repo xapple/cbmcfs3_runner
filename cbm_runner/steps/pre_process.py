f98ed6c03fcc0970f4dd1e79140afc57d70cab67
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2019-04-01 11:35:24 +0200

    Switch M types to Sort_Type=2

--- cbm_runner/steps/pre_process.py
+++ cbm_runner/steps/pre_process.py
@@ -29,15 +29,15 @@ class PreProcessor(object):
     def __call__(self):
         self.filter_dist_events(self.paths.disturbance_events, self.paths.disturbance_events_filtered)
 
-    def filter_dist_events(self, old_path, new_path, remove_m_types=True):
+    def filter_dist_events(self, old_path, new_path):
         """The calibration database is configured to run over a period of
          100 years. We would like to limit the simulation to the historical
          period. Currently Year < 2015.
 
          Filtering M types on Sort Type 6 is necessary to avoid the famous error:
 
-            Error:  Illegal target type for RANDOM sort in timestep 3, action number 190, in disturbance group 1, sort typ
-    e 6, target type 2.
+            Error:  Illegal target type for RANDOM sort in timestep 3, action number 190,
+                    in disturbance group 1, sort type 6, target type 2.
             Error:  Invalid disturbances found in .\input\disturb.lst.  Aborting.
          """
         # Load the original dataframe #
@@ -45,7 +45,7 @@ class PreProcessor(object):
         # Filter rows #
         period_max = self.parent.base_year - self.parent.inventory_start_year + 1
         new_df = old_df.query("Step <= %s" % period_max)
-        # Special case #
-        if remove_m_types: new_df = new_df.query('Sort_Type != 6 | Measurement_type == "A"')
+        # Filtering M types #
+        new_df.loc[(new_df['Sort_Type'] == 6) & (new_df['Measurement_type'] == 'M'), 'Sort_Type'] = 2
         # Write the result #
         new_df.to_csv(str(new_path), index=False)
