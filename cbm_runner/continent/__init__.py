e8b63f091f704b0d2bc0d0873d97fae9f4f232e3
Paul Rougieux <paul.rougieux@gmail.com>
2019-04-01 14:22:47 +0000

    Replace 'orig' by 'input' as the sub-folder indicating we are in the presence of a CBM data folder

--- cbm_runner/continent/__init__.py
+++ cbm_runner/continent/__init__.py
@@ -34,7 +34,7 @@ class Continent(object):
         """Store the cbm_data_repos directory paths where there
         is a directory for every country."""
         self.cbm_data_repos = DirectoryPath(cbm_data_repos)
-        self.all_countries = [Country(d) for d in self.cbm_data_repos.flat_directories if 'orig' in d]
+        self.all_countries = [Country(d) for d in self.cbm_data_repos.flat_directories if 'input' in d]
 
     @property
     def input(self):
