e174755a4707fa9ae4e2439171b8fe4d33887c24
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2019-05-10 14:16:49 +0200

    Copy reports to local workstation

---
+++ scripts/others/copy_reports.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+A script to copy PDF reports from the distant machine to the local workstation.
+
+Typically you would run this file from a command line like this:
+
+     python3 ~/repos/cbmcfs3_runner/scripts/others/copy_reports.py
+"""
+
+# Built-in modules #
+import os
+
+# Third party modules #
+import pbs3
+
+# Constants #
+home = os.environ.get('HOME', '~') + '/'
+
+###############################################################################
+pbs3.rsync('-avz', 'paulaws:repos/cbmcfs3_data/reports/countries/',
+                   home + 'repos/cbmcfs3_data/reports/countries/')
