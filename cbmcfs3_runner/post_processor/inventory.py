cc6e9fb861e551113ebc8075b94bb8388c1528ab
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2019-05-15 17:16:33 +0200

    Summing inventories with different average ages

--- cbmcfs3_runner/post_processor/inventory.py
+++ cbmcfs3_runner/post_processor/inventory.py
@@ -11,12 +11,55 @@ Unit D1 Bioeconomy.
 # Built-in modules #
 
 # Third party modules #
+import pandas, numpy
 
 # First party modules #
 from plumbing.cache import property_cached
 from autopaths.auto_paths import AutoPaths
 
-# Internal modules #
+###############################################################################
+# This value is in years and needs to be confimed with Scott
+CBM_BIN_WIDTH = 20.0
+
+# This value is in years and can be changed to adjust the precision of the discretisation
+CBM_PRECISION = 0.01
+
+def categorical_to_discrete(bin_center, bin_height, bin_width, precision):
+    """This function is more or less the inverse of the pandas.cut method.
+    Starting with binned data, we will assume a uniform distribution and
+    transform it back to discrete data with a given precision.
+
+    Given the center and width of a bin we will create a numpy vector
+    with conservation of total mass (i.e. bin_height).
+
+    We will check that we left bound cannot exceed zero.
+
+    >>> categorical_to_discrete(4, 5, 6, 1)
+    array([0., 0., 0., 5., 5.])
+    >>> categorical_to_discrete(1, 6, 10, 1)
+    array([1., 1., 1., 1., 1., 1.])
+    """
+    # Round to precision #
+    bin_radius = bin_width / 2
+    bin_radius = int(numpy.round(bin_radius / precision))
+    bin_center = int(numpy.round(bin_center / precision))
+    # Edges #
+    bin_left   = bin_center - bin_radius
+    bin_right  = bin_center + bin_radius
+    # Check that the left edge is never negative #
+    bin_left = max(bin_left, 0)
+    # Build vector like this: 0,0,0,0,1,1,1,1,1,1,1,1 #
+    vector = numpy.append(numpy.zeros(bin_left), numpy.ones(bin_right - bin_left))
+    # Multiply by the matching height #
+    vector *= bin_height / (bin_right - bin_left)
+    # Check mass is conserved #
+    numpy.testing.assert_allclose(vector.sum(),  bin_height)
+    # Return #
+    return vector
+
+def apply_discretizer(row, key):
+    """Given a row from our dataframe, return the discretized vector."""
+    return categorical_to_discrete(row['AveAge'], row[key], CBM_BIN_WIDTH, CBM_PRECISION)
 
 ###############################################################################
 class Inventory(object):
@@ -105,3 +148,31 @@ class Inventory(object):
         df['Merch_Vol_ha'] = df.Merch_C_ha / df.db
         # Return result #
         return df
+
+    @property_cached
+    def grouped(self):
+        """
+        Group the simulated inventory data frame and collapse columns.
+        """
+        def aggregator(df):
+            """The input df is a data frame with multiple rows and
+            the grouping columns still present. One must return a
+            series (i.e. only one row) with the grouping columns absent."""
+            # Get the discretized version of all rows #
+            all_vectors = df.apply(apply_discretizer, axis=1, key='Area')
+            all_vectors = pandas.DataFrame(v for v in all_vectors).fillna(0.0)
+            # Sum them up #
+            final_vector = all_vectors.sum()
+            # Return #
+            return pandas.Series(dict(area_per_age=final_vector))
+        # Columns we will keep #
+        group_cols = ['TimeStep', 'forest_type']
+        # Do it #
+        df = (self.simulated
+              .groupby(group_cols)
+              .agg(aggregator)
+              .reset_index())
+        # Return #
+        return df
+
+###############################################################################
