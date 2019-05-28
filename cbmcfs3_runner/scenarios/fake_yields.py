089ab6fb7392c2ea0c1970258377fdd8000a5403
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2019-05-28 15:06:55 +0200

    Better fake yield scenario (bis bis)

--- cbmcfs3_runner/scenarios/fake_yields.py
+++ cbmcfs3_runner/scenarios/fake_yields.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 
 """
-This scenarios is the same as static_demand.py except that we use the
-current yield for the historical period
-We are not supposed to do this.
-It is just for comparison purposes, to see what effect has this change of yield.
+This scenario is the same as `static_demand` except that we use the
+current yield tables for the historical period simulation.
+We are normally not supposed to do this. But, for comparison purposes,
+we are going to do so anyway to see what effect has this change of yield.
 """
 
 # Built-in modules #
@@ -24,15 +24,17 @@ class FakeYields(Scenario):
     @property_cached
     def runners(self):
         """A dictionary of country codes with a list of runners (for each country)."""
+        # Create all runners #
         result = {c.iso2_code: [Runner(self, c, 0)] for c in self.continent}
+        # Modify these runners #
         for c in self.continent:
-            # Get runner #
-            runner = result[c.iso2_code][0]
+            # Get the runner of the last step #
+            runner = result[c.iso2_code][-1]
             # Copy the class attribute into the instance of the class #
             xls = runner.default_sit.create_xls
             xls.file_name_to_sheet_name = xls.file_name_to_sheet_name.copy()
             # Switch the relevant key #
-            xls.file_name_to_sheet_name.pop('historical_yields')
-            xls.file_name_to_sheet_name['yields'] = 'Growth'
+            growth = xls.file_name_to_sheet_name.pop('historical_yields')
+            xls.file_name_to_sheet_name['yields'] = growth
         # Return #
         return result
