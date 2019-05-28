c936fa9c779638b0b1be71b25e7878c49d427165
Paul Rougieux <paul.rougieux@gmail.com>
2019-05-28 14:34:47 +0200

    Create scenario with future yields in the historical period

---
+++ cbmcfs3_runner/scenarios/fake_yields.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+This scenarios is the same as static_demand.py except that we use the 
+current yield for the historical period
+We are not supposed to do this. 
+It is just for comparison purposes, to see what effect has this change of yield. 
+"""
+
+# Built-in modules #
+
+# First party modules #
+from plumbing.cache import property_cached
+
+# Internal modules #
+from cbmcfs3_runner.scenarios.base_scen import Scenario
+from cbmcfs3_runner.core.runner import Runner
+
+###############################################################################
+class FakeYields(Scenario):
+    short_name = 'fake_yields'
+    
+
+    @property_cached
+    def runners(self):
+        """A dictionary of country codes with a list of runners (for each country)."""
+        result = {c.iso2_code: [Runner(self, c, 0)] for c in self.continent}
+        for c in self.continent:
+            runner = result[c.iso2_code][0]
+            # Copy the class attribute into the instance of the class
+            runner.default_sit.create_xls.file_name_to_sheet_name =  runner.default_sit.create_xls.file_name_to_sheet_name.copy()
+            runner.default_sit.create_xls.file_name_to_sheet_name.pop('historical_yields')                                                                                                      
+            runner.default_sit.create_xls.file_name_to_sheet_name['yields'] = 'Growth'     
+        return result
