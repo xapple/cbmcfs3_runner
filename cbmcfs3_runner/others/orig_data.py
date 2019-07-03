1258568bc625256f3e3e715685ec99ea23ba37cb
Paul Rougieux <paul.rougieux@gmail.com>
2019-07-03 16:23:44 +0200

    Add inventory and yields to orig_data

--- cbmcfs3_runner/others/orig_data.py
+++ cbmcfs3_runner/others/orig_data.py
@@ -18,6 +18,7 @@ from autopaths.auto_paths import AutoPaths
 from plumbing.cache import property_cached
 
 # Internal modules #
+from cbmcfs3_runner.others.common import reshape_yields_long
 
 ###############################################################################
 class OrigData(object):
@@ -48,12 +49,51 @@ class OrigData(object):
         return pandas.read_csv(str(self.paths[item]))
 
     @property_cached
+    def inventory(self):
+        """
+        Columns are:
+
+        ['status', 'forest_type', 'region', 'management_type',
+         'management_strategy', 'climatic_unit', 'conifers_bradleaves',
+         'UsingID', 'Age', 'Area', 'Delay', 'UNFCCCL', 'HistDist', 'LastDist']
+        """
+        df = self['inventory']
+        # Create the age_class column
+        # so it can be used as a join variable with a yields table
+        df['age_class'] = (df['Age']
+                           .replace('AGEID', '', regex=True)
+                           .astype('int'))
+        return df.rename(columns = self.classifiers_mapping)
+
+    @property_cached
+    def yields(self):
+        """
+        Columns are:
+
+        ['_1', '_2', '_3', '_4', '_5', '_6', '_7', 'Sp', 'Vol0', 'Vol1', 'Vol2',
+         'Vol3', 'Vol4', 'Vol5', 'Vol6', 'Vol7', 'Vol8', 'Vol9', 'Vol10',
+         'Vol11', 'Vol12', 'Vol13', 'Vol14', 'Vol15', 'Vol16', 'Vol17', 'Vol18',
+         'Vol19', 'Vol20', 'Vol21', 'Vol22', 'Vol23', 'Vol24', 'Vol25', 'Vol26',
+         'Vol27', 'Vol28', 'Vol29', 'Vol30']
+        """
+        df = self['yields']
+        return df.rename(columns = self.classifiers_mapping)
+
+    @property_cached
     def historical_yields(self):
-        """ Historical yield table"""
+        """ Historical yield taken from the xls_append object.
+            The object used to append historical yield
+            to the Standard Import Tool
+            for the carbon pool initialisation period"""
         df = self['historical_yields']
         # Rename classifier _1, _2, _3 to forest_type, region, etc. #
-        return df.rename(columns = self.parent.classifiers.mapping)
+        return df.rename(columns = self.classifiers_mapping)
 
     @property_cached
     def yields_long(self):
-        return self.reshape_yields_long(self.yields)
+        return reshape_yields_long(self.yields)
+
+    @property_cached
+    def historical_yields_long(self):
+        return reshape_yields_long(self.historical_yields)
+
