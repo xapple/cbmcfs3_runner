f1e9072133c2b08c6ffaa41f4d8df3a70bd97c26
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2019-07-23 14:58:44 +0200

    Added documentation to silviculture

--- cbmcfs3_runner/others/silviculture.py
+++ cbmcfs3_runner/others/silviculture.py
@@ -36,7 +36,12 @@ class Silviculture(object):
     To allocate the harvest across disturbance
     types (clear cut, thinning) and additional classifiers (forest type,
     management type, management strategy) we use a proportion method based
-    on the historical inventory and yield curve.
+    on the historical inventory and yield curve within a country.
+
+    In essence we will harvest proportionally to what is found in a country.
+    In terms of volume, if a country has 90% of firs, we will harvest 90% there.
+    Of course this is within the harvestable range, we will exclude trees that are
+    too young.
     """
 
     all_paths = """
@@ -58,7 +63,12 @@ class Silviculture(object):
 
     @property_cached
     def treatments(self):
-        """Load the CSV that is 'silv_treatments.csv'."""
+        """Load the CSV that is 'silv_treatments.csv'.
+        The column "Man_Nat" represents either 'Man-made disturbance'
+        or a Natural disturbance.
+        'Perc_merch_biom_rem' is redundant with 'Dist_ID' and simply shows
+        the percent of thinning.
+        """
         df = pandas.read_csv(str(self.paths.treatments))
         # Dist_Type_ID can be given as either a numeric or a character variable
         # convert to string to prevent issues when merging and filtering
@@ -83,6 +93,11 @@ class Silviculture(object):
         to the product that is harvested.
         The sub merchantable and snag volume 'Tot_V_SubMerch' and 'Tot_V_Snags'
         go to the corresponding fuel wood pool, either coniferous or broadleaved.
+
+        * IRW stands for Industrial Round Wood
+        * FW stands for Fuel Wood
+        * C stands for coniferous
+        * B stands for broadleaved
         """
         s = """     pool  |   HWP  |  co_product
              Tot_V_Merch  |  FW_C  |  FW_C
@@ -107,14 +122,16 @@ class Silviculture(object):
         Calculate the theoretical stock based on the inventory area
         by age class multiplied by the corresponding volume-per-hectare for
         each age class producing "stock" in terms of m^3.
-        
+
         Columns are: ['status', 'forest_type', 'region', 'management_type',
-                      'management_strategy', 'climatic_unit', 
-                      'conifers_bradleaves', 'age_class', 'Area', 'volume', 
+                      'management_strategy', 'climatic_unit',
+                      'conifers_bradleaves', 'age_class', 'Area', 'volume',
                       'stock', 'age']
         """
-        inventory = self.parent.orig_data.inventory
+        # Load data frames #
+        inventory     = self.parent.orig_data.inventory
         h_yields_long = self.parent.orig_data.historical_yields_long
+        # Index #
         index = ['status', 'forest_type', 'region', 'management_type',
                  'management_strategy', 'climatic_unit', 'conifers_bradleaves',
                  'age_class']
@@ -136,7 +153,7 @@ class Silviculture(object):
     @property_cached
     def stock_available_by_age(self):
         """Calculate the stock available based on the harvest proportion
-        in the silviculture_treatments table and multiply by a correction factor.
+        in the silviculture_treatments table and multiplied by a correction factor.
 
         Note: the harvest proportion in the silviculture_treatments table should be
         the same as the proportion going to products in the disturbance matrix.
@@ -144,20 +161,24 @@ class Silviculture(object):
 
         The status classifier does not have the same value between the data frames
         silviculture and stock_based_on_yield.
-        
+
         Columns are: ['forest_type', 'management_type', 'management_strategy',
-                      'conifers_bradleaves', 'status', 'region', 'climatic_unit', 
-                      'age_class', 'Area', 'volume', 'stock', 'age', 
-                      'Dist_Type_ID', 'Sort_Type', 'Efficency', 'Min_age', 
+                      'conifers_bradleaves', 'status', 'region', 'climatic_unit',
+                      'age_class', 'Area', 'volume', 'stock', 'age',
+                      'Dist_Type_ID', 'Sort_Type', 'Efficency', 'Min_age',
                       'Max_age', 'Min_since_last', 'Max_since_last', 'HWP',
                       'RegenDelay', 'ResetAge', 'Percent', 'WD', 'OWC_Perc',
                       'Snag_Perc', 'Perc_Merch_Biom_rem', 'Man_Nat', 'corr_fact',
                       'stock_available']
+
+        Min_since_last represents the minimum age since the last disturbance for
+        a new disturbance to be applied.
         """
         # Join with correction factor #
         silviculture = (self.treatments
                         .set_index('forest_type')
-                        .join(self.corr_fact.set_index('forest_type'))
+                        .join(self.corr_fact
+                              .set_index('forest_type'))
                         .reset_index())
         # Check that status is unique #
         if len(silviculture['status'].unique()) > 1:
@@ -188,11 +209,12 @@ class Silviculture(object):
     def stock_available_agg(self):
         """Aggregate stock_available_by_age and sum the stock available over
         all age classes. Some natural disturbances are ignored.
-        
+
         Columns are: ['forest_type', 'management_type', 'management_strategy',
                       'conifers_bradleaves', 'Dist_Type_ID', 'stock_available',
-                      'HWP', 'status'],
+                      'HWP', 'status']
         """
+        # Index #
         index = ['forest_type', 'management_type', 'management_strategy',
                  'conifers_bradleaves', 'Dist_Type_ID']
         # Aggregate #
@@ -219,55 +241,69 @@ class Silviculture(object):
 
     @property_cached
     def harvest_proportion(self):
-        """To allocate the harvest across 
-        disturbance types (clear cut, thining) 
+        """To allocate the harvest across
+        disturbance types (clear cut, thinning)
         and additional classifiers (forest type, management type,
-        management strategy) we use a proportion based 
+        management strategy) we use a proportion based
         on the historical inventory and yield curve.
 
-        The harvest proportion is calculated from the stock available 
+        The harvest proportion is calculated from the stock available
         by combining two information sources:
-            The inventory provides an area by forest type and age class
-            The growth/yield curve provides a volume by forest type and age class
-        We multiply those two values to obtain a 
-        stock = area * volume for each particular classifiers 
+
+            * The inventory provides an area by forest type and age class
+            * The growth/yield curve provides a volume by forest type and age class
+
+        We multiply those two values to obtain a
+        "stock = area * volume" for each particular classifiers
         and age class combination.
-        The stock available is then calculated based 
-        on the percentage moved in the disturbance matrix 
+
+        The stock available is then calculated based
+        on the percentage moved in the disturbance matrix
         (also available in the silviculture table Perc_Merch_Biom_rem)
-        and on an empirical harvest correction factor, 
+        and on an empirical harvest correction factor,
         for each disturbance and combination of classifiers:
-        stock available = stock * percentage harvested 
+
+            stock available = stock * percentage harvested
+
         by the disturbance * correction factor.
-        
-        The data frame below is the allocation of harvest 
-        along the classifiers used in self.stock_available_agg: 
+
+        The data frame below is the allocation of harvest
+        along the classifiers used in self.stock_available_agg:
+
             ['forest_type', 'management_type', 'management_strategy',
              'conifers_bradleaves', 'Dist_Type_ID' ].
-        The proportion is based on the available stock by 
+
+        The proportion is based on the available stock by
         harvested wood products (HWP) category.
-        
+
         Columns are: ['forest_type', 'management_type', 'management_strategy',
-                      'conifers_bradleaves', 'Dist_Type_ID', 'stock_available', 
+                      'conifers_bradleaves', 'Dist_Type_ID', 'stock_available',
                       'HWP', 'status', 'stock_tot', 'prop']
         """
+        # Load dataframe #
         df = self.stock_available_agg.copy()
+        # Add column stock_tot #
         df['stock_tot'] = df.groupby(['HWP'])['stock_available'].transform('sum')
-        df['prop'] = df['stock_available']/df['stock_tot']
+        # Add column prop #
+        df['prop']      = df['stock_available'] / df['stock_tot']
+        # Drop redundant total column #
         df.drop(columns=['stock_tot'])
-        return df.sort_values(by=['HWP'], ascending=False)
+        # Sort for readability #
+        df = df.sort_values(by=['HWP'], ascending=False)
+        # Return
+        return df
 
     @property_cached
     def harvest_proportion_legacy(self):
-        """Allocation of harvest along different classifiers 
+        """Allocation of harvest along different classifiers
         with time step included and redundant proportions for each time step.
         This table has a legacy structure based on Roberto's SAS code
         between "data IRW_C_Const;set HWP_Const_IRW_FW;"
         and     "data Stock_Prop_C_Const;*Proportion based on available Stock;"
-        
+
         Columns are: ['step', 'HWP', 'country', 'year', 'volume', 'forest_type',
-                      'management_type', 'management_strategy', 
-                      'conifers_bradleaves', 'Dist_Type_ID', 
+                      'management_type', 'management_strategy',
+                      'conifers_bradleaves', 'Dist_Type_ID',
                       'stock_available', 'status', 'stock_tot', 'prop']
         """
         # Join silv_stock on the HWP field.
