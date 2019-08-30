03a69aed6089ef3a6c4d53552fca924a63f01f8a
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2019-08-30 17:32:08 +0200

    Script to convert column case in input

--- scripts/orig/convert_column_case.py
+++ scripts/orig/convert_column_case.py
@@ -6,21 +6,16 @@ A script to convert the column names from CamelCase to snake_case.
 
 Typically you would run this file from a command line like this:
 
-     ipython3.exe -i -- /deploy/cbmcfs3_runner/scripts/orig/convet_column_case.py
+     ipython3.exe -i -- /deploy/cbmcfs3_runner/scripts/orig/convert_column_case.py
 """
 
 # Third party modules #
-
-# Internal modules #
-
-# Constants #
-
-###############################################################################
-# Third party modules #
+import pandas
 from tqdm import tqdm
 
 # First party modules #
 from autopaths.auto_paths import AutoPaths
+from plumbing.common import camel_to_snake
 
 # Internal modules #
 from cbmcfs3_runner.core.continent import continent
@@ -30,13 +25,13 @@ from cbmcfs3_runner.core.continent import continent
 ###############################################################################
 class CaseConverter(object):
     """
-    This class takes the file "calibration.mdb" as input and generates CSVs
-    from it.
+    This class takes many of the CSV files in export/ and orig/ and
+    converts their title case.
     """
 
     all_paths = """
     /orig/coefficients.csv
-    /export/
+    /orig/silv_treatments.csv
     /export/ageclass.csv
     /export/inventory.csv
     /export/classifiers.csv
@@ -45,6 +40,7 @@ class CaseConverter(object):
     /export/transition_rules.csv
     /export/yields.csv
     /export/historical_yields.csv
+    /fusion/back_inventory_aws.csv
     """
 
     def __init__(self, country):
@@ -54,7 +50,15 @@ class CaseConverter(object):
         self.paths = AutoPaths(self.country.data_dir, self.all_paths)
 
     def __call__(self):
-        pass
+        for p in self.paths:
+            # Skip some fusion files #
+            if not p: continue
+            # Read #
+            df = pandas.read_csv(str(p))
+            # Change #
+            df = df.rename(columns = camel_to_snake)
+            # Write #
+            df.to_csv(str(p), index=False)
 
 ###############################################################################
 if __name__ == '__main__':
