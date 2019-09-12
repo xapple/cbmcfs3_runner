d5b8ec7943821abceb829d4685ee9ee8f8f3102d
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2019-09-12 16:55:26 +0200

    Rename script add 100

--- scripts/orig/add_999_to_dist_id.py
+++ scripts/orig/add_999_to_dist_id.py
@@ -8,7 +8,7 @@ See https://webgate.ec.europa.eu/CITnet/jira/browse/BIOECONOMY-228
 
 Typically you would run this file from a command line like this:
 
-     ipython3.exe -i -- /deploy/cbmcfs3_runner/scripts/orig/add_100_to_dist_id.py
+     ipython3.exe -i -- /deploy/cbmcfs3_runner/scripts/orig/add_999_to_dist_id.py
 """
 
 # Third party modules #
@@ -22,11 +22,11 @@ from cbmcfs3_runner.core.continent import continent
 from autopaths.auto_paths import AutoPaths
 
 ###############################################################################
-class Add100ToDistID(object):
+class Add999ToDistID(object):
     """
     This class takes the file "disturbance_types.csv" and
     modifies it, by duplicating some entries with a new ID.
-    Why do we have to do this? Because of arcane CBM-CFS3
+    Why do we have to do this? Because of some arcane CBM-CFS3
     requirements.
     """
 
@@ -56,7 +56,9 @@ class Add100ToDistID(object):
         self.dists = [self.df.loc[dist][0] for dist in dist_to_dup]
         self.dup   = pandas.DataFrame(zip(dist_to_dup, self.dists), columns=[col_name, 'name'])
         # Add 100 #
-        self.dup[col_name] = self.dup[col_name].apply(lambda x: '1' + x)
+        self.dup[col_name] = self.dup[col_name].apply(lambda x: '999' + x)
+        # Add 'Future' #
+        pass
         # Write back to disk #
         self.df = self.df.reset_index()
         self.df = pandas.concat([self.df, self.dup])
@@ -64,5 +66,5 @@ class Add100ToDistID(object):
 
 ###############################################################################
 if __name__ == '__main__':
-    modifiers = [Add100ToDistID(c) for c in continent]
+    modifiers = [Add999ToDistID(c) for c in continent]
     for modifier in tqdm(modifiers): modifier()
