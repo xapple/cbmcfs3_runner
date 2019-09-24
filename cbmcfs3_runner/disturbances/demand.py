a855ff08f2436c8c5001faa0cab639e0eb828206
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2019-09-24 15:09:10 +0200

    Some cosmetic changes

--- cbmcfs3_runner/disturbances/demand.py
+++ cbmcfs3_runner/disturbances/demand.py
@@ -14,25 +14,27 @@ Unit D1 Bioeconomy.
 from plumbing.cache import property_cached
 
 # Third party modules #
-import pandas
-import math
-import numpy
+import pandas, math, numpy
 
 # Internal modules #
 from cbmcfs3_runner import module_dir
+from cbmcfs3_runner.pump.common import left_join
 
 # Constants #
 historical_demand_path = module_dir + 'extra_data/hist_harvest_corrected.csv'
-gftm_irw_demand_path = module_dir + 'extra_data/gftm_forest_model.csv'
-gftm_fw_demand_path = module_dir + 'extra_data/gftm_fuel_wood_bau.csv'
+gftm_irw_demand_path   = module_dir + 'extra_data/gftm_forest_model.csv'
+gftm_fw_demand_path    = module_dir + 'extra_data/gftm_fuel_wood_bau.csv'
 
 # Parse #
 historical_demand = pandas.read_csv(str(historical_demand_path))
-gftm_irw_demand = pandas.read_csv(str(gftm_irw_demand_path), header=None)
-gftm_fw_demand = pandas.read_csv(str(gftm_fw_demand_path))
-gftm_fw_demand['country_iso2'] = (gftm_fw_demand['country_iso2']
-                                  .replace(['LUX', 'SW', 'SL'], 
-                                           ['LU', 'SE', 'SI']))
+gftm_irw_demand   = pandas.read_csv(str(gftm_irw_demand_path), header=None)
+gftm_fw_demand    = pandas.read_csv(str(gftm_fw_demand_path))
+
+# Fix some country names #
+old_names = ['LUX', 'SW', 'SL']
+new_names = ['LU',  'SE', 'SI']
+fixed_names = gftm_fw_demand['country_iso2'].replace(old_names, new_names)
+gftm_fw_demand['country_iso2'] = fixed_names
 
 ###############################################################################
 class Demand(object):
@@ -57,27 +59,28 @@ class Demand(object):
     unknown reason.
     """
 
+    bark_correction_factor = 0.88
+
     def __init__(self, parent):
         # Default attributes #
         self.parent = parent
-        self.bark_correction_factor = 0.88
 
     @property_cached
     def year_expansion(self):
-        """Create a little data frame to expand years 
+        """Create a little data frame to expand years
         from a 5 year time frame to a yearly time frame.
-        
+
         GFTM gives a yearly demand over a 5 year interval.
         We will use this data frame to duplicate the yearly demand
         volume 5 times. This will then be used to generate disturbances.
-        
+
         Columns are : ['year_min', 'year']
         """
         year_min = numpy.concatenate([numpy.repeat(x,5) for x in range(2016, 2030, 5)])
         df = pandas.DataFrame({'year_min': year_min,
-                                           'year':     range(2016, 2031, 1)})
+                               'year':     range(2016, 2031, 1)})
         df['year'] = df['year'].astype(int)
-        # Convert year to time step
+        # Convert year to time step #
         df['step'] = self.parent.year_to_timestep(df['year'])
         return df
 
@@ -98,8 +101,7 @@ class Demand(object):
     @property_cached
     def gftm_irw(self):
         """Future IRW demand as predicted by GFTM.
-        Load and reshape the data frame in long format.
-        """
+        Load and reshape the data frame in long format."""
         # Fill values #
         header = self.gftm_header.fillna(method='ffill', axis=1)
         # Get the headers #
@@ -122,25 +124,25 @@ class Demand(object):
               .rename(columns = {0: 'variable',
                                  1: 'year',
                                  2: 'product'}))
-        # Correct an inconsistency in the year range
+        # Correct an inconsistency in the year range #
         df['year'] = df['year'].replace('2016to 2020', '2016 to 2020')
-        # Filter for years with data and select the variable of interest
+        # Filter for years with data and select the variable of interest #
         years_to_keep = ['2016 to 2020', '2021 to 2025', '2026 to 2030']
         variable      = 'Annual  production (m3ub) - from GFTM'
         df = df.query("variable==@variable & year in @years_to_keep").copy()
-        # Convert under bark demand volumes to over bark using a correction factor
+        # Convert under bark demand volumes to over bark using a correction factor #
         df['value_ob'] = df['value'] / self.bark_correction_factor
-        # Sum log and pulpwood
-        # Create a little data frame to rename products to HWP
+        # Sum log and pulpwood #
+        # Create a little data frame to rename products to HWP #
         gftm_irw_names = pandas.DataFrame({'product': ['C log', 'C pulpwood',
                                                        'N log', 'N pulpwood'],
                                            'hwp':     ['irw_c', 'irw_c',
                                                        'irw_b', 'irw_b']})
         df = (df
-              # Rename products to HWP
+              # Rename products to HWP #
               .set_index('product')
               .join(gftm_irw_names.set_index('product'))
-              # Aggregate the log and pulpwood values by HWP
+              # Aggregate the log and pulpwood values by HWP #
               .groupby(['year', 'hwp'])
               .agg({'value_ob': sum})
               .reset_index())
@@ -151,42 +153,38 @@ class Demand(object):
         df = df.rename(columns={'year':'year_text'})
         # Repeat lines for each successive year within a range by
         # joining the year_expansion data frame
-        # columns_of_interest = ['hwp', 'year_min', 'value_ob']
-        # df = df[columns_of_interest]
-        df = (df
-              .set_index(['year_min'])
-              .join(self.year_expansion.set_index(['year_min'])))
+        df = left_join(df, self.year_expansion, 'year_min')
         return df
 
     @property_cached
     def gftm_fw(self):
         """Future FW demand as predicted by GFTM.
-        Load and reshape the data frame in long format.
-        """
-        # Get the row corresponding to the current country.
+        Load and reshape the data frame in long format."""
+        # Get the row corresponding to the current country #
         selector = gftm_fw_demand['country_iso2'] == self.parent.iso2_code
+        # Check there is something to find for this country #
         if not any(selector):
-            msg = f'No fuel wood data for {self.parent.iso2_code} in '
-            msg += f'"{gftm_fw_demand_path}".'
+            msg = f'No fuel wood data for {self.parent.iso2_code} ' \
+                  f'in "{gftm_fw_demand_path}".'
             raise pandas.errors.EmptyDataError(msg)
+        # Copy only the current country #
         fw_wide = gftm_fw_demand.loc[selector].copy()
-        # Reshape to long format
-        df = fw_wide.melt(id_vars='country_iso2', var_name='product_year', 
+        # Reshape to long format #
+        df = fw_wide.melt(id_vars='country_iso2', var_name='product_year',
                           value_name='value')
-        # Separate the hwp and year_min columns 
+        # Separate the hwp and year_min columns  #
         df['hwp'], df['year_min'] = df['product_year'].str.split('_', 1).str
-        df['hwp'] = df['hwp'].replace(['Coniferous', 'Broadleaved'], 
+        # Rename #
+        df['hwp'] = df['hwp'].replace(['coniferous', 'broadleaved'],
                                       ['fw_c', 'fw_b'])
         df['year_min'] = df['year_min'].astype(int) + 1
-        # limit fw year to 2026 equal to the maximum of irw years
+        # Limit fw year to 2026 equal to the maximum of irw years #
         df = df.query('year_min<2030').copy()
-        # Convert demand value to over bark 
+        # Convert demand value to over bark #
         df['value_ob'] = df['value'] / self.bark_correction_factor
         # Repeat lines for each successive year within a range by
         # joining the year_expansion data frame
-        df = (df
-              .set_index(['year_min'])
-              .join(self.year_expansion.set_index(['year_min'])))
+        df = left_join(df, self.year_expansion, 'year_min')
         return df
 
     @property_cached
@@ -207,17 +205,17 @@ class Demand(object):
         df = self.historical_wide.melt(id_vars    = ['country', 'step', 'year'],
                                        var_name   = 'hwp',
                                        value_name = 'volume')
-        # Make HWP uppercase to match the silviculture table
+        # Make HWP uppercase to match the silviculture table #
         df['hwp'] = df['hwp'].str.upper()
         # Check if the total column matches with the sum of the other columns
         # Set abs_tol=1 to one to allow for rounding errors in the input data
-        sum_tot = df.query("hwp=='TOTAL'")['volume'].sum()
+        sum_tot   = df.query("hwp=='TOTAL'")['volume'].sum()
         sum_other = df.query("hwp!='TOTAL'")['volume'].sum()
         if not math.isclose(sum_tot, sum_other, abs_tol=1):
             msg = "The sum of the total column: %s "
             msg += "doesn't match the sum of the other columns: %s"
             raise Exception(msg % (sum_tot, sum_other))
-        # Remove the total column from the table
+        # Remove the total column from the table #
         df = df.query("hwp!='TOTAL'").copy()
         # Sort #
         df = df.sort_values(by=['year', 'hwp'], ascending=[True, False])
