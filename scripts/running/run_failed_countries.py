e15a96c2c6306ef703cc5433fab3ac327297ec7d
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2019-10-18 16:47:18 +0200

    Script to run only failed countries

---
+++ scripts/running/run_failed_countries.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+A script to rerun only the countries that didn't pass.
+
+Typically you would run this file from a command line like this:
+
+     ipython3.exe -i -- /deploy/cbmcfs3_runner/scripts/running/run_failed_countries.py
+"""
+
+# Built-in modules #
+
+# Third party modules #
+from tqdm import tqdm
+
+# First party modules #
+
+# Internal modules #
+from cbmcfs3_runner.core.continent import continent
+
+###############################################################################
+scenario       = continent.scenarios['static_demand']
+runners        = [r[-1] for k,r in scenario.runners.items()]
+failed_runners = [r for r in runners if r.map_value < 1.0]
+for r in tqdm(failed_runners): r(interrupt_on_error=False)
+
+###############################################################################
+for r in failed_runners:
+    print("---------------")
+    print(r.country.iso2_code)
+    print(r.tail)
