319195ffaa31dddd41a39149276c237841fc1f9d
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2021-07-21 13:08:46 +0200

    Update add_eighth_classifier.py

--- scripts/orig/add_eighth_classifier.py
+++ scripts/orig/add_eighth_classifier.py
@@ -4,7 +4,8 @@
 """
 A script to add an eighth classifier to every country in order to separate
 the initialization period from the historical period. This is useful for
-instance to switch all the yield curves of every species as soon as we hit 1995.
+instance to switch all the yield curves of every species as soon as we hit
+the country start year.
 """
 
 # Built-in modules #
@@ -23,18 +24,19 @@ from cbmcfs3_runner.core.continent import continent
 class ClassifierAdder(object):
     """
     This class takes many of the CSV files in "export/" and changes them.
+    It adds an eighth classifier.
 
+    * The classifier is called "yield_period" (it could be just called period).
+    * The values are "init", "cur" for the new classifier (see `timeline.svg`).
     """
 
     all_paths = """
-    /export/ageclass.csv
-    /export/inventory.csv
     /export/classifiers.csv
     /export/disturbance_events.csv
-    /export/disturbance_types.csv
     /export/transition_rules.csv
     /export/yields.csv
     /export/historical_yields.csv
+    /export/inventory.csv
     """
 
     def __init__(self, country):
@@ -46,17 +48,66 @@ class ClassifierAdder(object):
     def __call__(self):
         # Record header #
         self.record_header()
-        # Loop #
-        for p in self.paths:
-            # Read into memory #
-            df = pandas.read_csv(str(p))
-            # Change #
-            df = df
-            # Write back to disk #
-            df.to_csv(str(p), index=False, float_format='%g')
+        # All files #
+        self.mod_classifiers()
+        self.mod_events()
+        self.mod_rules()
+        self.mod_ylds()
+        self.mod_hist_ylds()
+        self.mod_inv()
         # Restore header #
         self.restore_header()
 
+    #-------------------------- Every file  -----------------------------------#
+    def mod_classifiers(self):
+        # Read into memory #
+        df = pandas.read_csv(str(p))
+        # Change #
+        df = df
+        # Write back to disk #
+        df.to_csv(str(p), index=False, float_format='%g')
+
+    def mod_events(self):
+        # Read into memory #
+        df = pandas.read_csv(str(p))
+        # Change #
+        df = df
+        # Write back to disk #
+        df.to_csv(str(p), index=False, float_format='%g')
+
+    def mod_rules(self):
+        # Read into memory #
+        df = pandas.read_csv(str(p))
+        # Change #
+        df = df
+        # Write back to disk #
+        df.to_csv(str(p), index=False, float_format='%g')
+
+    def mod_ylds(self):
+        # Read into memory #
+        df = pandas.read_csv(str(p))
+        # Change #
+        df = df
+        # Write back to disk #
+        df.to_csv(str(p), index=False, float_format='%g')
+
+    def mod_hist_ylds(self):
+        # Read into memory #
+        df = pandas.read_csv(str(p))
+        # Change #
+        df = df
+        # Write back to disk #
+        df.to_csv(str(p), index=False, float_format='%g')
+
+    def mod_inv(self):
+        # Read into memory #
+        df = pandas.read_csv(str(p))
+        # Change #
+        df = df
+        # Write back to disk #
+        df.to_csv(str(p), index=False, float_format='%g')
+
+    #-------------------------- Post-processing -------------------------------#
     def restore_header(self):
         """
         In a pandas dataframe, the column names have to be unique, because
@@ -64,11 +115,12 @@ class ClassifierAdder(object):
         "transition_rules", column names are repeated. So we have to restore
         these headers afterwards.
         """
-        self.header
+        self.paths.rules.remove_first_line()
+        self.paths.rules.prepend(self.header)
 
     def record_header(self):
         """Keep the first line of the file "transition_rules" in memory."""
-        self.header =
+        self.header = self.paths.rules.first
 
 ###############################################################################
 if __name__ == '__main__':
