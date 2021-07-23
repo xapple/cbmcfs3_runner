d89e9b9b7e00ea5443fc07c0a223bee0b2134230
Paul Rougieux <paul.rougieux@gmail.com>
2021-07-23 11:52:54 +0200

    Export the simulated inventory to csv files. Use try and except statement to Ignore GB.

--- scripts/export/create_output_csv.py
+++ scripts/export/create_output_csv.py
@@ -35,6 +35,17 @@ for scen_name in tqdm(scenarios, desc='Scenarios'):
     # Loop runners #
     for runners in tqdm(scenario.runners.values(), desc='Countries', leave=False):
         r = runners[-1]
-        r.post_processor.csv_maker()
-    # Make zip file #
+        # Do not recreate the csv file if it already exists
+        if r.post_processor.csv_maker.paths.inventory_simulated.exists:
+            print(f"Skipping {r.country.iso2_code}")
+            continue
+        print(f"Processing {r.country.iso2_code}")
+        try:
+            r.post_processor.csv_maker()
+        except Exception as e:
+            print("no data in ", r.country.iso2_code)
+            print('Error loading data: '+ str(e))
+    # Make zip files #
+    scenario.make_csv_zip('inventory_simulated', '~/exports/for_sarah/' + scen_name + '/')
     scenario.make_csv_zip('ipcc_pools', '~/exports/for_sarah/' + scen_name + '/')
+
