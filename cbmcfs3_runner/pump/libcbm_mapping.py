2134cbdaedc759e9e71c8a8f01081fa8e0c46158
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2021-10-20 21:31:43 +0200

    Better docstrings

--- cbmcfs3_runner/pump/libcbm_mapping.py
+++ cbmcfs3_runner/pump/libcbm_mapping.py
@@ -11,9 +11,8 @@ Provides access to a mapping table between cbmcfs3 pool names and libcbm pool na
 
 You can use this object like this:
 
-    from cbmcfs3_runner.pump.libcbm_mapping import libcbm_mapping
-    print(libcbm_mapping)
-
+    >>> from cbmcfs3_runner.pump.libcbm_mapping import libcbm_mapping
+    >>> print(libcbm_mapping)
 """
 
 # Third party modules #
@@ -22,8 +21,7 @@ import pandas
 # Internal modules #
 from cbmcfs3_runner import module_dir
 
-
+# Load a dataframe #
 libcbm_mapping_path = module_dir + 'extra_data/libcbm_pools.csv'
-
 libcbm_mapping = pandas.read_csv(libcbm_mapping_path)
 
