7f9e41c0bbf643d03c73afaac0a6d4e46b0da014
Paul Rougieux <paul.rougieux@gmail.com>
2021-10-21 12:46:46 +0200

    Map cbmcfs3 turnover rates to libcbm ones in a correspondance table

--- cbmcfs3_runner/pump/libcbm_mapping.py
+++ cbmcfs3_runner/pump/libcbm_mapping.py
@@ -11,8 +11,12 @@ Provides access to a mapping table between cbmcfs3 pool names and libcbm pool na
 
 You can use this object like this:
 
-    >>> from cbmcfs3_runner.pump.libcbm_mapping import libcbm_mapping
-    >>> print(libcbm_mapping)
+    >>> from cbmcfs3_runner.pump import libcbm_mapping
+    >>> print("Pools mapping")
+    >>> print(libcbm_mapping.pools)
+    >>> print("Turnover rates mapping")
+    >>> print(libcbm_mapping.turnover_rates)
+
 """
 
 # Third party modules #
@@ -21,7 +25,10 @@ import pandas
 # Internal modules #
 from cbmcfs3_runner import module_dir
 
-# Load a dataframe #
-libcbm_mapping_path = module_dir + 'extra_data/libcbm_pools.csv'
-libcbm_mapping = pandas.read_csv(libcbm_mapping_path)
+# Load a correspondance table between cbmcfs3 and libcbm pool names
+pools = pandas.read_csv(module_dir + 'extra_data/libcbm_pools.csv')
+# Keep a legacy name for compatibility purposes
+libcbm_mapping = pools
 
+# Load a correspondance table between cbmcfs3 and libcbm turnover rates
+turnover_rates = pandas.read_csv(module_dir + 'extra_data/libcbm_turnover_rates.csv')
