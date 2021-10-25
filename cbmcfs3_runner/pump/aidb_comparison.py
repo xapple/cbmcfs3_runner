ec532819e0a086077475bfd479836a378f187f6f
Viorel Blujdea <Viorel.Blujdea@ec.europa.eu>
2021-10-25 16:15:53 +0200

    cleaned up the method for decay_rate_comparison

--- cbmcfs3_runner/pump/aidb_comparison.py
+++ cbmcfs3_runner/pump/aidb_comparison.py
@@ -97,7 +97,7 @@ class CompareAIDB(object):
 
         # Load cbmcfs3 turnover
         cfs3_turnover = self.cbmcfs3_aidb.database['tblecoboundarydefault']
-
+        
         # Combine the two
         index = ['eco_boundary_id', 'eco_boundary_name']
         # Reshape to long format
@@ -151,54 +151,46 @@ class CompareAIDB(object):
         soc_pools_lib = self.libcbm_aidb.db.read_df('dom_pool')
         soc_pools_lib.rename(columns={"id": "dom_pool_id"}, inplace=True)
         dom_pools_lib = soc_pools_lib.merge(pools_lib, how ='inner', on="pool_id")
-        #print(pools_lib.head(1), end = '*************\n')
-        #print(soc_pools_lib.head(1), end = '*************\n')
-        #print(dom_pools_lib.head(0), end = '*************\n')
-        
+                
         # New library
         #libcbm
         decay_lib = self.libcbm_aidb.db.read_df('decay_parameter')
         decay_lib = decay_lib.merge(dom_pools_lib, how="inner", on="dom_pool_id")
         decay_lib = decay_lib[['pool_id', 'code', 'base_decay_rate', 'reference_temp', 'q10', 'prop_to_atmosphere', 'max_rate']]
-   #print(decay_lib.head(5),end = '*Q*Q*Q*Q*Q*Q*Q*Q*Q*Q*Q* \n')
         
         #cbmcfs3
         decay_cfs = self.cbmcfs3_aidb.database['tbldomparametersdefault']
         decay_cbm = decay_cfs.merge(cbmcfs3_pools, how ='inner', on="soil_pool_id")
-        #decay_cbm = decay_cfs3.rename(columns={'soil_pool_id':'dom_pool_id',
-        #                                       'organic_matter_decay_rate':'base_decay_rate',
-        #                                       'reference_temp':'reference_temp',
-        #                                       'max_decay_rate_soft':'max_rate'})
-        # decay_cbm= decay_cbm[['pool_id', 'code', 'base_decay_rate', 'reference_temp', 'q10', 'prop_to_atmosphere', 'max_rate']]
-        #decay_cbm = decay_cbm.rename(columns={'max_decay_rate_hard':'max_rate'})
-   #print(decay_cbm.head(5), end = '*X*X*X*X*X*X*X*X*X*X*X* \n')
-        
+        decay_cbm = decay_cbm[['pool_id', 'code', 'organic_matter_decay_rate', 'reference_temp',   'q10','max_decay_rate_soft',  'max_decay_rate_hard',  'prop_to_atmosphere' ]]
+           
         #Combine the two
         index = ['pool_id', 'code']
+        
         # Reshape to long format
         lib_decay_long = (decay_lib
                      .melt(id_vars=index, var_name='libcbm', value_name='libcbm_value'))
         cfs3_decay_long = (decay_cbm
                     .melt(id_vars=index, var_name='cbmcfs3', value_name='cbmcfs3_value'))
-        # Join cbmcfs3 table with with the maping table then with the libcbm table
+        
+        # Join cbmcfs3 table with the maping table, then with the libcbm table
         combined = cfs3_decay_long.merge(decay_rates, on="cbmcfs3")
         combined = combined.merge(lib_decay_long, on=index + ["libcbm"], how="left")
+        
         
         # Join tables using to consecutive join instructions
         combined['diff'] = combined["cbmcfs3_value"] - combined["libcbm_value"]
         combined['diff'].unique()
-        # print(combined)
         return combined
         
     def check_decay_parameters(self, threshold=1e-9):
-        """Check that the absolute value of the difference between the turnover parameters is below the given threshold"""
+        """Check that the absolute value of the difference between the turnover parameters is below 1e-9"""
         df_decay = self.load_decay_rates()
         all_equal = all(df_decay['diff'].abs() < threshold)
         if all_equal:
-            print(f"{self.iso2_code} turnover rates are identical in libcbm and cbmcfs3")
+            print(f"{self.iso2_code} decay rates are identical in libcbm and cbmcfs3")
         else:
             problem_rows = df_decay[df_decay['diff'].abs() > threshold]
-            msg = f"Turnover parameter mismatch in '{self.iso2_code}'\n{problem_rows}.\n"
+            msg = f"decay parameter mismatch in '{self.iso2_code}'\n{problem_rows}.\n"
             msg += "You can load the comparison table with: \n"
             msg += f"compare = CompareAIDB(cbmcfs3_continent.countries['{self.iso2_code}'])\n"
             msg += "df = compare.load_turnover_parameters()\n"
